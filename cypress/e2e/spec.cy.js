ca3ff700d7ce1cdd86738a9154d29de4901407ee
vivek <konavivekramakrishna@gmail.com>
2024-02-12 16:21:08 +0530

    checking incorrect tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -60,7 +60,7 @@ describe("Todo App Test,", () => {
     cy.get('button[type="submit"]').click();
     cy.wait(500);
     cy.get(".Todo-Item").should("exist");
-    cy.get("#count-due-today").contains("1");
+    cy.get("#count-due-today").contains("6");
   });
 
   it("Delete Todo", () => {
